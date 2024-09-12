3c912fc50561336b4fa9d69487384830456166bb
Vishal Pansuriya <125434786+VishalP-1308@users.noreply.github.com>
2024-09-12 12:53:19 +0530

    Create middleware.ts

---
+++ Frontend/middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (!token) {
+    const url = new URL("/login", req.url);
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/users/:path*", "/posts/:path*", "/user:path", "/:path"],
+};
